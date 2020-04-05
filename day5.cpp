ce3ab5b1a0db710d36978e2d66fbb1b3a9649081
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2020-04-05 14:48:51 +0530

    :tada: solved buy-and-sell-stocks-2 problem

--- day5.cpp
+++ day5.cpp
@@ -11,7 +11,7 @@ class Solution {
         int max = 0;
         for (int i = 1; i < prices.size(); i++) {
             if (prices[i] > prices[i - 1]) {
-                max += prices[i] - prices[i - 1];
+                max = max + prices[i] - prices[i - 1];
             }
         }
 
