afe1ef42f17877e2fe05c4ae9f470c5729981a83
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2020-04-28 20:27:12 +0530

    :cake: solved first unique number in O(1) time & O(N) memory

---
+++ day-28.cpp
@@ -0,0 +1,92 @@
+/*
+First Unique Number
+You have a queue of integers, you need to retrieve the first unique integer in the queue.
+
+Implement the FirstUnique class:
+
+FirstUnique(int[] nums) Initializes the object with the numbers in the queue.
+int showFirstUnique() returns the value of the first unique integer of the queue, and returns -1 if there is no such integer.
+void add(int value) insert value to the queue.
+
+Example 1:
+
+Input: 
+["FirstUnique","showFirstUnique","add","showFirstUnique","add","showFirstUnique","add","showFirstUnique"]
+[[[2,3,5]],[],[5],[],[2],[],[3],[]]
+Output: 
+[null,2,null,2,null,3,null,-1]
+
+Explanation: 
+FirstUnique firstUnique = new FirstUnique([2,3,5]);
+firstUnique.showFirstUnique(); // return 2
+firstUnique.add(5);            // the queue is now [2,3,5,5]
+firstUnique.showFirstUnique(); // return 2
+firstUnique.add(2);            // the queue is now [2,3,5,5,2]
+firstUnique.showFirstUnique(); // return 3
+firstUnique.add(3);            // the queue is now [2,3,5,5,2,3]
+firstUnique.showFirstUnique(); // return -1
+
+Example 2:
+Input: 
+["FirstUnique","showFirstUnique","add","add","add","add","add","showFirstUnique"]
+[[[7,7,7,7,7,7]],[],[7],[3],[3],[7],[17],[]]
+Output: 
+[null,-1,null,null,null,null,null,17]
+
+Explanation: 
+FirstUnique firstUnique = new FirstUnique([7,7,7,7,7,7]);
+firstUnique.showFirstUnique(); // return -1
+firstUnique.add(7);            // the queue is now [7,7,7,7,7,7,7]
+firstUnique.add(3);            // the queue is now [7,7,7,7,7,7,7,3]
+firstUnique.add(3);            // the queue is now [7,7,7,7,7,7,7,3,3]
+firstUnique.add(7);            // the queue is now [7,7,7,7,7,7,7,3,3,7]
+firstUnique.add(17);           // the queue is now [7,7,7,7,7,7,7,3,3,7,17]
+firstUnique.showFirstUnique(); // return 17
+
+Example 3:
+
+Input: 
+["FirstUnique","showFirstUnique","add","showFirstUnique"]
+[[[809]],[],[809],[]]
+Output: 
+[null,809,null,-1]
+
+Explanation: 
+FirstUnique firstUnique = new FirstUnique([809]);
+firstUnique.showFirstUnique(); // return 809
+firstUnique.add(809);          // the queue is now [809,809]
+firstUnique.showFirstUnique(); // return -1
+*/
+
+// O(1) & O(N) solution
+class FirstUnique {
+    unordered_map<int, list<int>::iterator> hashMap;
+    list<int> keys;
+
+   public:
+    FirstUnique(vector<int>& nums) {
+        for (int elt : nums) {
+            add(elt);
+        }
+    }
+
+    int showFirstUnique() {
+        if (keys.size() > 0) return keys.front();
+        return -1;
+    }
+
+    void add(int value) {
+        if (hashMap.find(value) != hashMap.end()) {
+            list<int>::iterator it = hashMap[value];
+            if (it != keys.end()) {
+                keys.erase(it);
+                hashMap[value] = keys.end();
+            }
+        } else {
+            keys.push_back(value);
+            list<int>::iterator it = keys.end();
+            it--;
+            hashMap[value] = it;
+        }
+    }
+};
