04b737df8f0b159ea7ff0711b5860ab2a7f9b0ac
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2020-05-19 20:24:41 +0530

    :dizzy: solved online stock span

---
+++ day-49.cpp
@@ -0,0 +1,48 @@
+/*
+  Online Stock Span
+
+Write a class StockSpanner which collects daily price quotes for some stock, and returns the span of that stock's price for the current day.
+
+The span of the stock's price today is defined as the maximum number of consecutive days (starting from today and going backwards) for which the price of the stock was less than or equal to today's price.
+
+For example, if the price of a stock over the next 7 days were [100, 80, 60, 70, 60, 75, 85], then the stock spans would be [1, 1, 1, 2, 1, 4, 6].
+
+ 
+
+Example 1:
+
+Input: ["StockSpanner","next","next","next","next","next","next","next"], [[],[100],[80],[60],[70],[60],[75],[85]]
+Output: [null,1,1,1,2,1,4,6]
+Explanation: 
+First, S = StockSpanner() is initialized.  Then:
+S.next(100) is called and returns 1,
+S.next(80) is called and returns 1,
+S.next(60) is called and returns 1,
+S.next(70) is called and returns 2,
+S.next(60) is called and returns 1,
+S.next(75) is called and returns 4,
+S.next(85) is called and returns 6.
+
+Note that (for example) S.next(75) returned 4, because the last 4 prices
+(including today's price of 75) were less than or equal to today's price.
+*/
+
+// O(N) & O(N) solution
+class StockSpanner {
+   public:
+    stack<pair<int, int> > prices;
+    StockSpanner() {
+        prices = stack<pair<int, int> >();
+    }
+
+    int next(int price) {
+        int sum = 1;
+        while (!prices.empty() && price >= prices.top().first) {
+            sum += prices.top().second;
+            prices.pop();
+        }
+
+        prices.push({price, sum});
+        return sum;
+    }
+};
