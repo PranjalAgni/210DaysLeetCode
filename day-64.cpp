d6c0bf93cde5047e2858a68230ef87148133d470
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2020-06-03 23:13:58 +0530

    :rocket: solved two city scheduling

---
+++ day-64.cpp
@@ -0,0 +1,47 @@
+/*
+  Two City Scheduling
+
+There are 2N people a company is planning to interview. The cost of flying the i-th person to city A is costs[i][0], and the cost of flying the i-th person to city B is costs[i][1].
+
+Return the minimum cost to fly every person to a city such that exactly N people arrive in each city.
+
+ 
+
+Example 1:
+
+Input: [[10,20],[30,200],[400,50],[30,20]]
+Output: 110
+Explanation: 
+The first person goes to city A for a cost of 10.
+The second person goes to city A for a cost of 30.
+The third person goes to city B for a cost of 50.
+The fourth person goes to city B for a cost of 20.
+
+The total minimum cost is 10 + 30 + 50 + 20 = 110 to have half the people interviewing in each city.
+
+*/
+
+// O(N) & O(1) solution
+class Solution {
+    struct Comp {
+        bool operator()(vector<int>& a, vector<int>& b) {
+            return (a[0] - a[1]) < (b[0] - b[1]);
+        }
+    };
+
+   public:
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+        int answer = 0;
+        sort(costs.begin(), costs.end(), Comp());
+
+        for (int i = 0; i < costs.size() / 2; i++) {
+            answer += costs[i][0];
+        }
+
+        for (int i = costs.size() / 2; i < costs.size(); i++) {
+            answer += costs[i][1];
+        }
+
+        return answer;
+    }
+};
