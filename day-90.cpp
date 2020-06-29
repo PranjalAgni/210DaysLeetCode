30cdd431799083bd9f199464b1b06305822b5140
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2020-06-29 23:26:54 +0530

    :art: solved unique paths bottom-up DP

---
+++ day-90.cpp
@@ -0,0 +1,49 @@
+/*
+  Unique Paths
+
+
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+Example 1:
+
+Input: m = 3, n = 2
+Output: 3
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Right -> Down
+2. Right -> Down -> Right
+3. Down -> Right -> Right
+Example 2:
+
+Input: m = 7, n = 3
+Output: 28
+
+*/
+
+// Bottom up simple DP solutions
+// O(row * cols) both time & space complexity
+class Solution {
+   public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n));
+
+        for (int i = 0; i < n; i++) {
+            dp[m - 1][i] = 1;
+        }
+
+        for (int i = 0; i < m; i++) {
+            dp[i][n - 1] = 1;
+        }
+
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
+                dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+            }
+        }
+
+        return dp[0][0];
+    }
+};
