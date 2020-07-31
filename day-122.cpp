b8c802eb97506ca18e23801a2ac9f0ef8d5a03e5
PranjalAgni <pranjal.agnihotri6@gmail.com>
2020-07-31 23:24:12 +0530

    ✅ solved climbing stairs leetcode July Challenge completed

---
+++ day-122.cpp
@@ -0,0 +1,45 @@
+/*
+You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you
+climb to the top?
+
+Example 1:
+
+Input: 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+
+
+Constraints:
+
+1 <= n <= 45
+
+*/
+
+// Easy dp solution
+class Solution {
+   public:
+    int climbStairs(int n) {
+        vector<int> dp(n + 1);
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int steps = 2; steps <= n; steps++) {
+            dp[steps] = dp[steps - 1] + dp[steps - 2];
+        }
+
+        return dp[n];
+    }
+};
