c9ccd52b415cb1bca8ee0fa0779ce381c67fa7a4
PranjalAgni <pranjal.agnihotri6@gmail.com>
2020-09-18 23:36:50 +0530

    :rocket: solved best time to buy and sell stock

---
+++ day-171.cpp
@@ -0,0 +1,44 @@
+/*
+  Best Time to Buy and Sell Stock
+
+
+Say you have an array for which the ith element is the price of a given stock on
+day i.
+
+If you were only permitted to complete at most one transaction (i.e., buy one
+and sell one share of the stock), design an algorithm to find the maximum
+profit.
+
+Note that you cannot sell a stock before you buy one.
+
+Example 1:
+
+Input: [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit =
+6-1 = 5. Not 7-1 = 6, as selling price needs to be larger than buying price.
+Example 2:
+
+Input: [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
+
+*/
+
+// Simple O(N) approach, keeping track of minPrice and totalMaxPrice we got
+class Solution {
+   public:
+    int maxProfit(vector<int>& prices) {
+        int N = prices.size();
+        if (N < 2) return 0;
+        int maxProfit = 0;
+        int minPrice = prices[0];
+
+        for (int idx = 1; idx < N; idx++) {
+            maxProfit = max(maxProfit, prices[idx] - minPrice);
+            minPrice = min(minPrice, prices[idx]);
+        }
+
+        return maxProfit;
+    }
+};
