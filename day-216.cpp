5f55012327a4f70c6584b174e8b5f3cabfae9549
PranjalAgni <pranjal.agnihotri6@gmail.com>
2020-11-02 21:51:12 +0530

    :rocket: solved insertion sort list

---
+++ day-216.cpp
@@ -0,0 +1,54 @@
+/*
+  Insertion Sort List
+
+
+Sort a linked list using insertion sort.
+
+
+A graphical example of insertion sort. The partial sorted list (black) initially
+contains only the first element in the list. With each iteration one element
+(red) is removed from the input data and inserted in-place into the sorted list
+
+
+Algorithm of Insertion Sort:
+
+Insertion sort iterates, consuming one input element each repetition, and
+growing a sorted output list. At each iteration, insertion sort removes one
+element from the input data, finds the location it belongs within the sorted
+list, and inserts it there. It repeats until no input elements remain.
+
+Example 1:
+
+Input: 4->2->1->3
+Output: 1->2->3->4
+Example 2:
+
+Input: -1->5->3->4->0
+Output: -1->0->3->4->5
+
+ */
+
+// Simple linked list sorting
+class Solution {
+   public:
+    ListNode* insertionSortList(ListNode* head) {
+        vector<int> nums;
+        ListNode* temp = head;
+        while (temp != NULL) {
+            nums.push_back(temp->val);
+            temp = temp->next;
+        }
+
+        sort(nums.begin(), nums.end());
+
+        temp = head;
+        int idx = 0;
+        while (temp != NULL) {
+            temp->val = nums[idx];
+            temp = temp->next;
+            idx += 1;
+        }
+
+        return head;
+    }
+};
