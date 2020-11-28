f13572cab60656aa4002cb8dcbaf9f90e820ad0f
PranjalAgni <pranjal.agnihotri6@gmail.com>
2020-11-28 23:57:49 +0530

    ☂️  solved Sliding Window Maximum

---
+++ day-242.cpp
@@ -0,0 +1,71 @@
+/*
+Sliding Window Maximum
+
+Solution
+You are given an array of integers nums, there is a sliding window of size k
+which is moving from the very left of the array to the very right. You can only
+see the k numbers in the window. Each time the sliding window moves right by one
+position.
+
+Return the max sliding window.
+
+
+
+Example 1:
+
+Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+Output: [3,3,5,5,6,7]
+Explanation:
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+
+
+Example 2:
+Input: nums = [1], k = 1
+Output: [1]
+Example 3:
+
+Input: nums = [1,-1], k = 1
+Output: [1,-1]
+Example 4:
+
+Input: nums = [9,11], k = 2
+Output: [11]
+Example 5:
+
+Input: nums = [4,-2], k = 2
+Output: [4]
+
+
+Constraints:
+
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+1 <= k <= nums.length
+
+*/
+
+// Solved using sliding window scan + deque
+class Solution {
+   public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> ans;
+        deque<int> q;
+        for (int i = 0; i < nums.size(); i++) {
+            while (!q.empty() && q.back() < nums[i]) q.pop_back();
+            q.push_back(nums[i]);
+            if (i >= k - 1) ans.push_back(q.front());
+            if (i >= k - 1) {
+                if (q.front() == nums[i - k + 1]) q.pop_front();
+            }
+        }
+
+        return ans;
+    }
+};
