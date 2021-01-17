3c08b13d6cd6dbc17cfe33836fc55e48c2965586
PranjalAgni <pranjal.agnihotri6@gmail.com>
2021-01-17 23:57:18 +0530

    :sunflower: solved Count Sorted Vowel Strings

---
+++ day-291.cpp
@@ -0,0 +1,49 @@
+/*
+Count Sorted Vowel Strings
+
+Solution
+Given an integer n, return the number of strings of length n that consist only
+of vowels (a, e, i, o, u) and are lexicographically sorted.
+
+A string s is lexicographically sorted if for all valid i, s[i] is the same as
+or comes before s[i+1] in the alphabet.
+
+
+
+Example 1:
+
+Input: n = 1
+Output: 5
+Explanation: The 5 sorted strings that consist of vowels only are
+["a","e","i","o","u"]. Example 2:
+
+Input: n = 2
+Output: 15
+Explanation: The 15 sorted strings that consist of vowels only are
+["aa","ae","ai","ao","au","ee","ei","eo","eu","ii","io","iu","oo","ou","uu"].
+Note that "ea" is not a valid string since 'e' comes after 'a' in the alphabet.
+Example 3:
+
+Input: n = 33
+Output: 66045
+
+
+Constraints:
+
+1 <= n <= 50
+*/
+
+// We can assume that there would be 4 transition states
+// i.e. from a->e->i->o->u
+// and along with these transitions there would be n vowels in sorted order
+// hence we have total n+4 locations and after choosing 4 of them
+// (for the transitions to take place)
+// the elements at all other positions got fixed
+// hence the answer is n+4 C 4.
+
+class Solution {
+   public:
+    int countVowelStrings(int n) {
+        return (n + 1) * (n + 2) * (n + 3) * (n + 4) / 24;
+    }
+};
