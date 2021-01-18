1c830612f8b034951a5443f608256959beadb89f
PranjalAgni <pranjal.agnihotri6@gmail.com>
2021-01-18 23:52:00 +0530

    :zap: solved Max Number of K-Sum Pairs

---
+++ day-292.cpp
@@ -0,0 +1,64 @@
+/*
+Max Number of K-Sum Pairs
+
+Solution
+You are given an integer array nums and an integer k.
+
+In one operation, you can pick two numbers from the array whose sum equals k and
+remove them from the array.
+
+Return the maximum number of operations you can perform on the array.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,4], k = 5
+Output: 2
+Explanation: Starting with nums = [1,2,3,4]:
+- Remove numbers 1 and 4, then nums = [2,3]
+- Remove numbers 2 and 3, then nums = []
+There are no more pairs that sum up to 5, hence a total of 2 operations.
+Example 2:
+
+Input: nums = [3,1,3,4,3], k = 6
+Output: 1
+Explanation: Starting with nums = [3,1,3,4,3]:
+- Remove the first two 3's, then nums = [1,4,3]
+There are no more pairs that sum up to 6, hence a total of 1 operation.
+
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 109
+1 <= k <= 109
+
+*/
+
+// Simple O(NlogN) solution
+// SC: O(1)
+class Solution {
+   public:
+    int maxOperations(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int start = 0;
+        int end = nums.size() - 1;
+        int count = 0;
+
+        while (start < end) {
+            int sum = nums[start] + nums[end];
+
+            if (sum > k) {
+                end--;
+            } else if (sum < k) {
+                start++;
+            } else {
+                end--;
+                start++;
+                count++;
+            }
+        }
+        return count;
+    }
+};
